da3f7258c5da76a3e900ac10a435ed79a45e9b41
adawag-jef <epyadawag@gmail.com>
2021-09-12 12:16:01 +0800

    working with sprites

--- sprite-example.py
+++ sprite-example.py
@@ -1,7 +1,7 @@
 import pygame
 
-WIDTH = 360
-HEIGHT = 480
+WIDTH = 800
+HEIGHT = 600
 FPS = 30
 
 WHITE = (255, 255, 255)
@@ -10,6 +10,20 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((50, 50))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.rect.center = (WIDTH / 2, HEIGHT / 2)
+
+    def update(self):
+        self.rect.x += 5
+        if self.rect.left > WIDTH:
+            self.rect.right = 0
+
+
 pygame.init()
 pygame.mixer.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -17,6 +31,9 @@ pygame.display.set_caption('My Game')
 clock = pygame.time.Clock()
 
 all_sprites = pygame.sprite.Group()
+player = Player()
+
+all_sprites.add(player)
 
 running = True
 
@@ -27,8 +44,10 @@ while running:
         if event.type == pygame.QUIT:
             running = False
     
+    # update
     all_sprites.update()
 
+    # render
     screen.fill(BLACK)
     all_sprites.draw(screen)
 
